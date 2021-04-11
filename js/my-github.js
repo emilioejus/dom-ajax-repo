cc145e989831506cad919e743333560f7d536582
emilioejus <emilioejus@gmail.com>
2021-04-11 05:09:46 +0200

    Pruebas

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,15 @@
 // Write code here to communicate with Github
+
+let lista = document.querySelector("#repos-list");
+async function fetch() {
+  const API = "https://api.github.com/users/emilioejus/repos";
+  resolve = await fetch(API);
+  data = await resolve.json();
+  data.forEach(rep => {
+    let li = document.createElement("li");
+    li.innerText = rep.name;
+    lista.append(li);
+  });
+}
+fetch();
+console.log(lista);
