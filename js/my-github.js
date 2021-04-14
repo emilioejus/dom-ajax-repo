e8d081fb19073825bbf843658619a8cae8d3826e
emilioejus <emilioejus@gmail.com>
2021-04-14 16:37:23 +0200

    agregando los requisitos del 1 al 3

--- js/my-github.js
+++ js/my-github.js
@@ -1,15 +1,30 @@
 // Write code here to communicate with Github
 
+let myGithub = document.querySelector("#my-github > .nav-link");
 let lista = document.querySelector("#repos-list");
-async function dfetch() {
-  const API = "https://api.github.com/users/emilioejus/repos";
-  resolve = await fetch(API);
-  data = await resolve.json();
-  data.forEach(rep => {
-    let li = document.createElement("li");
-    li.innerText = rep.name;
-    lista.append(li);
+let numRepos = document.querySelector("#repos-count");
+myGithub.onclick = event => {
+  event.preventDefault();
+  console.log(document.querySelectorAll("#repos-list li:not(.sub-title)"));
+  document.querySelectorAll("#repos-list li:not(.sub-title)").forEach(li => {
+    li.remove();
   });
-}
+  async function dfetch() {
+    const API = "https://api.github.com/users/emilioejus/repos";
+    resolve = await fetch(API);
+    data = await resolve.json();
+    numRepos.innerText = data.length;
+    data.forEach(rep => {
+      let li = document.createElement("li");
+      li.classList.add("list-group-item-warning");
+      let a = document.createElement("a");
+      a.innerText = rep.name;
+      a.href = rep.html_url;
+      li.append(a);
+      lista.append(li);
+    });
+  }
+  dfetch();
+};
 
-console.log(dfetch());
+//console.log(dfetch());
